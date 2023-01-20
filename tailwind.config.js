08fabb399676e66d24db88410a8cad6cba8e9641
AsahRizky <ardiansyahrizky200@gmail.com>
2023-01-20 19:34:28 +0700

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                blok: "#E6EFFC",
+                sign: "#A7F8F8",
+                signup: "#00B9B9",
+            },
+        },
+    },
+    plugins: [],
+};
